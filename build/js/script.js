b5d3f93d31fd41055996be1f003125a73d8e834e
Ray Villalobos <ray@planetoftheweb.com>
2020-04-09 19:54:03 -0400

    scrollmagic and other movement

--- build/js/script.js
+++ build/js/script.js
@@ -2,18 +2,8 @@ $(function () {
   // Look for monsters with the class of scroll trigger
   let monsterScroll = document.querySelectorAll('.monster.scroll-trigger')
   monsterScroll.forEach((item) => {
-    item.style.animationDelay = `${Math.random() * 0.75 + 0.5}s`
+    item.style.animationDelay = `${Math.random() * 0.25 + 0.5}s`
   })
-
-  function getMonsters() {
-    monsterScroll.forEach((item) =>
-      inViewport(item)
-        ? item.classList.add('visible')
-        : item.classList.remove('visible') && item.classList.add('invisible')
-    )
-    window.requestAnimationFrame(getMonsters)
-  }
-  window.requestAnimationFrame(getMonsters)
 
   function pageOffset() {
     return window.pageYOffset !== undefined
@@ -22,9 +12,24 @@ $(function () {
           .scrollTop
   }
 
+  let headerContent = document.querySelector('.header-content')
+  let nav = document.querySelector('.site-nav')
+  let headerCue = document.querySelector('.header-cue')
+  let meetMonsters = document.querySelector('#meet')
+  let navHeight = nav.scrollHeight
+
   function moveHeader() {
-    let headerContent = document.querySelector('.header-content')
     let top = pageOffset()
+    let mainOnTop = meetMonsters.getBoundingClientRect().top - navHeight
+    mainOnTop < 0
+      ? nav.classList.add('in-body')
+      : nav.classList.remove('in-body')
+
+    let currentCuePosition = headerContent.getBoundingClientRect().top
+    currentCuePosition < -1
+      ? headerCue.classList.add('d-none')
+      : headerCue.classList.remove('d-none')
+
     headerContent.style.transform = `translate3D( 0px, ${top / 1.5}px, 0px)`
     headerContent.style.opacity =
       1 - Math.max(top / (window.innerHeight * 0.6), 0)
@@ -41,4 +46,24 @@ $(function () {
       (rect.top >= 0 && rect.bottom <= window.innerHeight)
     )
   }
+
+  function cycleThroughMonsters() {
+    monsterScroll.forEach((item) =>
+      inViewport(item)
+        ? item.classList.add('visible')
+        : item.classList.remove('visible') && item.classList.add('invisible')
+    )
+    window.requestAnimationFrame(cycleThroughMonsters)
+  }
+  window.requestAnimationFrame(cycleThroughMonsters)
+
+  let controller = new ScrollMagic.Controller()
+
+  // create a scene
+  let scene = new ScrollMagic.Scene({
+    triggerElement: '#pin-trigger',
+    duration: 500,
+  }).setPin('#types')
+
+  controller.addScene(scene)
 })
