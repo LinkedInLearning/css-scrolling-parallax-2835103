31134f9602f92b3a786ea6928aa5a80c0d497639
Ray Villalobos <ray@planetoftheweb.com>
2020-04-09 03:31:34 -0400

    manual parallax

--- build/js/script.js
+++ build/js/script.js
@@ -1,22 +1,38 @@
 $(function () {
-  let scroll = window.requestAnimationFrame
-  let el = document.querySelectorAll('.scroll-trigger')
-
-  el.forEach((item) => {
+  // Look for monsters with the class of scroll trigger
+  let monsterScroll = document.querySelectorAll('.monster.scroll-trigger')
+  monsterScroll.forEach((item) => {
     item.style.animationDelay = `${Math.random() * 0.75 + 0.5}s`
   })
 
-  let content = document.querySelectorAll('.scroll-trigger')
-
-  function loop() {
-    el.forEach((item) =>
+  function getMonsters() {
+    monsterScroll.forEach((item) =>
       inViewport(item)
         ? item.classList.add('visible')
         : item.classList.remove('visible') && item.classList.add('invisible')
     )
-    scroll(loop)
+    window.requestAnimationFrame(getMonsters)
+  }
+  window.requestAnimationFrame(getMonsters)
+
+  function pageOffset() {
+    return window.pageYOffset !== undefined
+      ? window.pageYOffset
+      : (document.documentElement || document.body.parentNode || document.body)
+          .scrollTop
   }
 
+  function moveHeader() {
+    let headerContent = document.querySelector('.header-content')
+    let top = pageOffset()
+    headerContent.style.transform = `translate3D( 0px, ${top / 1.5}px, 0px)`
+    headerContent.style.opacity =
+      1 - Math.max(top / (window.innerHeight * 0.6), 0)
+    window.requestAnimationFrame(moveHeader)
+  }
+  window.requestAnimationFrame(moveHeader)
+
+  // Detect wether an element is showing in the viewport
   function inViewport(el) {
     let rect = el.getBoundingClientRect()
     return (
@@ -25,6 +41,4 @@ $(function () {
       (rect.top >= 0 && rect.bottom <= window.innerHeight)
     )
   }
-
-  loop()
 })
