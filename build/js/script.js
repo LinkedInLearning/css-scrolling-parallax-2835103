e1b834fb3d3496af09dfad7e6ee4080d69d67100
Ray Villalobos <ray@planetoftheweb.com>
2020-04-18 05:55:27 -0400

    Scrolling Past meet

--- build/js/script.js
+++ build/js/script.js
@@ -1,8 +1,15 @@
 $(function () {
+  let nav = document.querySelector('.site-nav')
   let meetMonsters = document.querySelector('#meet')
+  let navHeight = nav.scrollHeight
 
   function moveHeader() {
-    console.log(`${meetMonsters.getBoundingClientRect().top}`)
+    let mainOnTop = meetMonsters.getBoundingClientRect().top - navHeight
+
+    mainOnTop < 0
+      ? nav.classList.add('in-body')
+      : nav.classList.remove('in-body')
+
     window.requestAnimationFrame(moveHeader)
   }
 
